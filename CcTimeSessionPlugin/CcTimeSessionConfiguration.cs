9a78ac9245843ba9ecfd283429b7a1f1b28a32ca
JeremPlt-Kraken <peltierj57@gmail.com>
2024-12-16 15:37:14 +0100

    Add documentation to config

--- CcTimeSessionPlugin/CcTimeSessionConfiguration.cs
+++ CcTimeSessionPlugin/CcTimeSessionConfiguration.cs
@@ -7,8 +7,11 @@ namespace CcTimeSessionPlugin;
 [UsedImplicitly(ImplicitUseKindFlags.Assign, ImplicitUseTargetFlags.WithMembers)]
 public class CcTimeSessionConfiguration : IValidateConfiguration<CcTimeSessionConfigurationValidator>
 {
+    [YamlMember(Description = "Session duration in minutes")]
     public int SessionTimeMinutes { get; init; } = 30;
+    [YamlMember(Description = "The unique ID that will be sent as part of the API POST request")]
     public string? SessionId { get; init; } = null;
+    [YamlMember(Description = "Language to use for sending broadcast messages")]
     public string? Language { get; init; } = "en";
 
     [YamlIgnore] public int SessionTimeSeconds => SessionTimeMinutes * 60;
