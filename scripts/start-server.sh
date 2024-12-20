f7e866db7dc852b351ccfb9078353b801d304551
JeremPlt-Kraken <peltierj57@gmail.com>
2024-12-20 19:56:20 +0100

    Update script

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-cd /app/acServer
-
-echo "Creating directories 'content' and 'cfg'..."
-mkdir -p content cfg
-
-# Copie le contenu de /shared-mods vers /app/acServer/content
-echo "Copying mods from /shared-mods to /app/acServer/content..."
-cp -R /shared-mods/* content/
+cd /app/AssettoServer
 
 # Copie le contenu de /shared-config vers /app/acServer/cfg
 echo "Copying config from /shared-config to /app/acServer/cfg..."
-cp /shared-config/* cfg/
+cp /shared-config/* .
 
 # Lance le serveur Assetto Corsa
 echo "Starting Assetto Corsa Server..."
