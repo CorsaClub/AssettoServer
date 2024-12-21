53618a8875ecb04e22ac30e2d67ba1045540bf47
JeremPlt-Kraken <peltierj57@gmail.com>
2024-12-21 15:17:35 +0100

    Add README.md and modify CcTimeSession name in logs
    CorsaConnect -> CorsaClub

--- CcTimeSessionPlugin/CcTimeSession.cs
+++ CcTimeSessionPlugin/CcTimeSession.cs
@@ -23,38 +23,38 @@ public class CcTimeSession : CriticalBackgroundService, IAssettoServerAutostart
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         var totalTimeSeconds = _configuration.SessionTimeSeconds;
-        
+
         const int tenMinuteInterval = 10 * 60;
         const int fiveMinuteInterval = 5 * 60;
         const int oneMinuteInterval = 60;
-        
+
         const int thirtyMinutes = 30 * 60;
         const int fifteenMinutes = 15 * 60;
         const int fiveMinutes = 5 * 60;
 
-        for (var remainingSeconds = totalTimeSeconds; remainingSeconds >= 0; )
+        for (var remainingSeconds = totalTimeSeconds; remainingSeconds >= 0;)
         {
             if (stoppingToken.IsCancellationRequested) break;
 
             _remainingSeconds = remainingSeconds;
 
             // Choisissez le message en fonction du temps restant.
-            var message = remainingSeconds == 0 
-                ? " [CorsaConnect] - Fin de session" 
-                : $" [CorsaConnect] - Il reste {remainingSeconds / 60} minutes.";
-            
+            var message = remainingSeconds == 0
+                ? " [CorsaClub] - Fin de session"
+                : $" [CorsaClub] - Il reste {remainingSeconds / 60} minutes.";
+
             _entryCarManager.BroadcastPacket(new ChatMessage { SessionId = 255, Message = message });
             Log.Information("Remaining time of session : {time} minutes", remainingSeconds / 60);
-        
-            if(remainingSeconds == 0)
+
+            if (remainingSeconds == 0)
             {
                 EndSession();
                 break;
             }
-            
+
             var sleepInterval = remainingSeconds switch
             {
-                >= thirtyMinutes => tenMinuteInterval, 
+                >= thirtyMinutes => tenMinuteInterval,
                 <= fifteenMinutes and > fiveMinutes => fiveMinuteInterval,
                 <= fiveMinutes and > 0 => oneMinuteInterval,
                 _ => remainingSeconds
