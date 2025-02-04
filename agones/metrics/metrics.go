2e6ebf3817e07a66d9ad5675e073136c5fb3bcce
JeremPlt-Kraken <peltierj57@gmail.com>
2025-02-04 18:16:44 +0100

    Fix double metrics and refactor it

--- agones/metrics/metrics.go
+++ agones/metrics/metrics.go
@@ -1,4 +1,4 @@
-// Package metrics contains Prometheus metric definitions for the server
+// Package metrics provides Prometheus metrics for the Assetto Corsa server.
 package metrics
 
 import (
@@ -6,172 +6,208 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
-// ServerLabels defines the common labels for all metrics
+// ServerLabels defines common labels for all server metrics
 var ServerLabels = []string{"server_id", "server_name", "server_type"}
 
-// Server state metrics
+// Basic server metrics
 var (
+	// ServerStateGauge tracks the current state of the server
 	ServerStateGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
 		Name: "assetto_server_state",
-		Help: "Current state of the server (0: Starting, 1: Ready, 2: Allocated, 3: Reserved, 4: Shutdown)",
+		Help: "Current state of the server (0=starting, 1=ready, 2=allocated, 3=reserved, 4=shutdown)",
 	}, ServerLabels)
 
+	// PlayersGauge tracks the current number of connected players
 	PlayersGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "assetto_server_players_total",
-		Help: "Current number of players on the server",
+		Name: "assetto_server_players",
+		Help: "Current number of connected players",
 	}, ServerLabels)
 
-	PlayerConnectCounter = promauto.NewCounterVec(prometheus.CounterOpts{
-		Name: "assetto_server_player_connects_total",
-		Help: "Total number of player connections since server start",
-	}, ServerLabels)
-
-	PlayerDisconnectCounter = promauto.NewCounterVec(prometheus.CounterOpts{
-		Name: "assetto_server_player_disconnects_total",
-		Help: "Total number of player disconnections since server start",
-	}, ServerLabels)
+	// ServerErrorsCounter tracks the number of server errors
+	ServerErrorsCounter = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name: "assetto_server_errors_total",
+		Help: "Total number of server errors",
+	}, append(ServerLabels, "error_type"))
 )
 
-// Session metrics
+// Health and performance metrics
 var (
-	SessionChangeCounter = promauto.NewCounterVec(prometheus.CounterOpts{
-		Name: "assetto_server_session_changes_total",
-		Help: "Total number of session changes",
+	// HealthPingFailuresCounter tracks failed health checks
+	HealthPingFailuresCounter = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name: "assetto_server_health_ping_failures_total",
+		Help: "Total number of failed health pings",
 	}, ServerLabels)
 
-	SessionDurationGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "assetto_server_session_duration_seconds",
-		Help: "Duration of the current session in seconds",
-	}, append(ServerLabels, "session_type"))
-
-	SessionStartTimeGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "assetto_server_session_start_timestamp",
-		Help: "Start timestamp of the current session",
-	}, append(ServerLabels, "session_type"))
-
-	SessionTimeLeftGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "assetto_server_session_time_left_seconds",
-		Help: "Time remaining in current session in seconds",
-	}, ServerLabels)
-)
-
-// Health metrics
-var (
+	// LastHealthPingGauge tracks the time since last successful health check
 	LastHealthPingGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
 		Name: "assetto_server_last_health_ping_seconds",
 		Help: "Time since last successful health ping in seconds",
 	}, ServerLabels)
 
-	HealthPingFailuresCounter = promauto.NewCounterVec(prometheus.CounterOpts{
-		Name: "assetto_server_health_ping_failures_total",
-		Help: "Total number of failed health pings",
-	}, ServerLabels)
-)
-
-// Error and authentication metrics
-var (
-	ServerErrorsCounter = promauto.NewCounterVec(prometheus.CounterOpts{
-		Name: "assetto_server_errors_total",
-		Help: "Total number of server errors detected",
-	}, append(ServerLabels, "error_type"))
-
-	AuthSuccessCounter = promauto.NewCounterVec(prometheus.CounterOpts{
-		Name: "assetto_server_auth_successes_total",
-		Help: "Total number of successful Steam authentications",
-	}, ServerLabels)
-)
-
-// Network metrics
-var (
-	NetworkBytesReceivedCounter = promauto.NewCounterVec(prometheus.CounterOpts{
-		Name: "assetto_server_network_bytes_received_total",
-		Help: "Total number of bytes received by the server",
-	}, ServerLabels)
-
-	NetworkBytesSentCounter = promauto.NewCounterVec(prometheus.CounterOpts{
-		Name: "assetto_server_network_bytes_sent_total",
-		Help: "Total number of bytes sent by the server",
-	}, ServerLabels)
-
-	NetworkLatencyGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "assetto_server_network_latency_ms",
-		Help: "Average network latency in milliseconds",
-	}, ServerLabels)
-)
-
-// Performance metrics
-var (
-	CpuUsageGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "assetto_server_cpu_usage_percent",
-		Help: "CPU usage percentage of the server process",
-	}, ServerLabels)
-
-	MemoryUsageGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "assetto_server_memory_usage_bytes",
-		Help: "Memory usage in bytes of the server process",
-	}, ServerLabels)
-)
-
-// Usage metrics
-var (
-	// Car metrics
-	CarUsageCounter = promauto.NewCounterVec(prometheus.CounterOpts{
-		Name: "assetto_server_car_usage_total",
-		Help: "Number of times each car has been used",
-	}, append(ServerLabels, "car_name"))
-
-	CarCountGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "assetto_server_car_count",
-		Help: "Number of cars per model currently in use",
-	}, append(ServerLabels, "car_model"))
-
-	// Track metrics
-	TrackUsageCounter = promauto.NewCounterVec(prometheus.CounterOpts{
-		Name: "assetto_server_track_usage_total",
-		Help: "Number of times each track has been used",
-	}, append(ServerLabels, "track_name"))
-
-	// Detailed player metrics
-	PlayerLatencyGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "assetto_server_player_latency_ms",
-		Help: "Player latency in milliseconds",
-	}, append(ServerLabels, "player_name", "steam_id"))
-
-	PlayerBestLapGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "assetto_server_player_best_lap_ms",
-		Help: "Player best lap time in milliseconds",
-	}, append(ServerLabels, "player_name", "steam_id"))
-
-	PacketLossGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "assetto_server_packet_loss_percent",
-		Help: "Current packet loss percentage",
-	}, append(ServerLabels, "player_name"))
-
-	// Track condition metrics
-	TrackGripGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "assetto_server_track_grip_level",
-		Help: "Current track grip level percentage",
-	}, ServerLabels)
-
-	TrackTemperatureGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "assetto_server_track_temperature_celsius",
-		Help: "Current track temperature in Celsius",
-	}, ServerLabels)
-
-	AirTemperatureGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "assetto_server_air_temperature_celsius",
-		Help: "Current air temperature in Celsius",
-	}, ServerLabels)
-
+	// TickRateGauge tracks the current server tick rate
 	TickRateGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
 		Name: "assetto_server_tick_rate",
 		Help: "Current server tick rate",
 	}, ServerLabels)
 )
 
-// Add more metrics useful for debugging
+// Resource usage metrics
 var (
-	// Metric to track command processing times
+	// CpuUsageGauge tracks CPU usage
+	CpuUsageGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "assetto_server_cpu_usage",
+		Help: "Current CPU usage percentage",
+	}, ServerLabels)
+
+	// MemoryUsageGauge tracks memory usage
+	MemoryUsageGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "assetto_server_memory_usage_bytes",
+		Help: "Current memory usage in bytes",
+	}, ServerLabels)
+)
+
+// Session metrics
+var (
+	// SessionDurationHistogram tracks session duration distribution
+	SessionDurationHistogram = promauto.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "assetto_server_session_duration_distribution_seconds",
+			Help:    "Distribution of session durations in seconds",
+			Buckets: prometheus.ExponentialBuckets(60, 2, 10), // Starting from 1 minute
+		},
+		append(ServerLabels, "session_type"),
+	)
+
+	// SessionDurationGauge tracks the current session duration
+	SessionDurationGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "assetto_server_session_duration_seconds",
+		Help: "Duration of the current session in seconds",
+	}, append(ServerLabels, "session_type"))
+
+	// SessionTimeLeftGauge tracks remaining session time
+	SessionTimeLeftGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "assetto_server_session_time_left_seconds",
+		Help: "Time remaining in the current session in seconds",
+	}, ServerLabels)
+
+	// SessionChangeCounter tracks session changes
+	SessionChangeCounter = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name: "assetto_server_session_changes_total",
+		Help: "Total number of session changes",
+	}, ServerLabels)
+)
+
+// Track condition metrics
+var (
+	// TrackGripGauge tracks track grip level
+	TrackGripGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "assetto_server_track_grip",
+		Help: "Current track grip level percentage",
+	}, ServerLabels)
+
+	// TrackTemperatureGauge tracks track temperature
+	TrackTemperatureGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "assetto_server_track_temperature",
+		Help: "Current track temperature in Celsius",
+	}, ServerLabels)
+
+	// AirTemperatureGauge tracks air temperature
+	AirTemperatureGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "assetto_server_air_temperature",
+		Help: "Current air temperature in Celsius",
+	}, ServerLabels)
+)
+
+// Track and car usage metrics
+var (
+	// TrackUsageCounter tracks how many times each track is used
+	TrackUsageCounter = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name: "assetto_server_track_usage_total",
+		Help: "Total number of times each track has been used",
+	}, append(ServerLabels, "track_name"))
+
+	// CarUsageCounter tracks how many times each car is used
+	CarUsageCounter = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name: "assetto_server_car_usage_total",
+		Help: "Total number of times each car has been used",
+	}, append(ServerLabels, "car_name"))
+)
+
+// Player metrics
+var (
+	// PlayerConnectCounter tracks player connections
+	PlayerConnectCounter = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name: "assetto_server_player_connects_total",
+		Help: "Total number of player connections",
+	}, ServerLabels)
+
+	// PlayerLatencyGauge tracks player latency
+	PlayerLatencyGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "assetto_server_player_latency_ms",
+		Help: "Current player latency in milliseconds",
+	}, append(ServerLabels, "player_name", "steam_id"))
+
+	// PacketLossGauge tracks player packet loss
+	PacketLossGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "assetto_server_player_packet_loss",
+		Help: "Current player packet loss percentage",
+	}, append(ServerLabels, "player_name", "steam_id"))
+
+	// PlayerBestLapGauge tracks player best lap times
+	PlayerBestLapGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "assetto_server_player_best_lap_ms",
+		Help: "Player best lap time in milliseconds",
+	}, append(ServerLabels, "player_name", "steam_id"))
+
+	// PlayerDisconnectCounter tracks player disconnections
+	PlayerDisconnectCounter = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name: "assetto_server_player_disconnects_total",
+		Help: "Total number of player disconnections",
+	}, ServerLabels)
+
+	// AuthSuccessCounter tracks successful authentications
+	AuthSuccessCounter = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name: "assetto_server_auth_success_total",
+		Help: "Total number of successful authentications",
+	}, ServerLabels)
+)
+
+// Server operation metrics
+var (
+	// ServerPortsGauge tracks server ports usage
+	ServerPortsGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "assetto_server_ports_total",
+		Help: "Current number of ports used by the server",
+	}, []string{"port_type", "port"})
+
+	// ServerUpdateRateGauge tracks server update rate
+	ServerUpdateRateGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "assetto_server_update_rate_seconds",
+		Help: "Current server update rate in seconds",
+	}, ServerLabels)
+
+	// LobbyRegistrationCounter tracks lobby registrations
+	LobbyRegistrationCounter = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name: "assetto_server_lobby_registrations_total",
+		Help: "Total number of lobby registrations",
+	}, ServerLabels)
+
+	// ServerStartCounter tracks server starts
+	ServerStartCounter = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name: "assetto_server_starts_total",
+		Help: "Total number of server starts",
+	}, ServerLabels)
+
+	// SessionEndCounter tracks session ends
+	SessionEndCounter = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name: "assetto_server_ends_total",
+		Help: "Total number of server ends",
+	}, ServerLabels)
+)
+
+// Debug metrics
+var (
+	// CommandProcessingTimeHistogram tracks command processing times
 	CommandProcessingTimeHistogram = promauto.NewHistogramVec(
 		prometheus.HistogramOpts{
 			Name:    "assetto_server_command_processing_seconds",
@@ -181,7 +217,7 @@ var (
 		append(ServerLabels, "command_type"),
 	)
 
-	// Metric to track player latency
+	// PlayerLatencyHistogram tracks player latency distribution
 	PlayerLatencyHistogram = promauto.NewHistogramVec(
 		prometheus.HistogramOpts{
 			Name:    "assetto_server_player_latency_distribution_ms",
@@ -192,40 +228,17 @@ var (
 	)
 )
 
+// Network metrics
 var (
-	ServerPortsGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "assetto_server_ports_total",
-		Help: "Current number of ports used by the server",
-	}, []string{"port_type", "port"})
-
-	ServerUpdateRateGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "assetto_server_update_rate_seconds",
-		Help: "Current server update rate in seconds",
+	// NetworkBytesReceivedCounter tracks received network traffic
+	NetworkBytesReceivedCounter = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name: "assetto_server_network_bytes_received_total",
+		Help: "Total number of bytes received",
 	}, ServerLabels)
 
-	LobbyRegistrationCounter = promauto.NewCounterVec(prometheus.CounterOpts{
-		Name: "assetto_server_lobby_registrations_total",
-		Help: "Total number of lobby registrations",
+	// NetworkBytesSentCounter tracks sent network traffic
+	NetworkBytesSentCounter = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name: "assetto_server_network_bytes_sent_total",
+		Help: "Total number of bytes sent",
 	}, ServerLabels)
-)
-
-var (
-	ServerStartCounter = promauto.NewCounterVec(prometheus.CounterOpts{
-		Name: "assetto_server_starts_total",
-		Help: "Total number of server starts",
-	}, ServerLabels)
-
-	SessionEndCounter = promauto.NewCounterVec(prometheus.CounterOpts{
-		Name: "assetto_server_ends_total",
-		Help: "Total number of server ends",
-	}, ServerLabels)
-
-	SessionDurationHistogram = promauto.NewHistogramVec(
-		prometheus.HistogramOpts{
-			Name:    "assetto_server_session_duration_seconds",
-			Help:    "Duration of the current session in seconds",
-			Buckets: prometheus.ExponentialBuckets(0.001, 2, 10),
-		},
-		append(ServerLabels, "session_type"),
-	)
 )
